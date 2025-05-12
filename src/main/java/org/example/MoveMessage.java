1e423b7ff17a8d792551394648aa9b40bcf345b4
Kyle Lin <kyle.lin@wirelesscar.com>
2025-05-12 17:57:21 +0800

    Fix

--- src/main/java/org/example/MoveMessage.java
+++ src/main/java/org/example/MoveMessage.java
@@ -1,10 +1,22 @@
 package org.example;
 
-public record MoveMessage(Vector2 from, Direction direction) {
+import io.netty.buffer.ByteBufAllocator;
+
+public record MoveMessage(Vector2 from, Direction direction) implements Message {
 
 
     public static MoveMessage create(int x, int y, int dir) {
         return new MoveMessage(new Vector2(x, y), Direction.fromValue(dir));
     }
 
+    @Override
+    public byte[] toBytes() {
+        byte[] bytes = new byte[16];
+        var buffer = ByteBufAllocator.DEFAULT.buffer();
+        buffer.writeInt(MessageType.Move.value());
+        buffer.writeInt(from().x());
+        buffer.writeInt(from().y());
+        buffer.writeInt(direction().value());
+        return bytes;
+    }
 }
